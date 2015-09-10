2c5d33282598cd7ae61f4208a352e5d5d9b50db4
Igor <9533121456@mail.ru>
2015-09-10 22:22:07 +0300

    project created.

---
+++ TestingSystem/Controllers/MainController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace TestingSystem.Controllers
+{
+    public class MainController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
